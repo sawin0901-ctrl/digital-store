b49f2c062cb30459593810d2e2396cdd55409c58
sawin0901-ctrl <sawin0901@gmail.com>
2026-06-19 13:55:34 +1000

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold">Digital Store</h1>
+      <p className="mt-4 text-gray-600">Добро пожаловать в магазин цифровых товаров</p>
+    </main>
+  )
+}
